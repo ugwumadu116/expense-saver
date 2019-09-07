1aafcce141f46f2be824e9b430db67015dd532b0
ugwumadu116 <ugwumadu116@gmail.com>
2019-09-07 02:09:00 +0100

    sd

---
+++ dist/precache-manifest.d1db11295bc8f6803425a6cd503082b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1b78c0f47b0a4f3416f5686c1b6570",
+    "url": "/myshaga/index.html"
+  },
+  {
+    "revision": "af910e3bbcc379ccf148",
+    "url": "/myshaga/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5931ebcf17235b854e5d",
+    "url": "/myshaga/static/js/2.49efe33e.chunk.js"
+  },
+  {
+    "revision": "af910e3bbcc379ccf148",
+    "url": "/myshaga/static/js/main.8554e7e2.chunk.js"
+  },
+  {
+    "revision": "14d8e96477a2b20f1d85",
+    "url": "/myshaga/static/js/runtime~main.2800afc4.js"
+  }
+]);
